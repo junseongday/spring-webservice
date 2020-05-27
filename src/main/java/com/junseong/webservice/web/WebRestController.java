b93a7ac3a0e447210ca259ae1ba61a09c1c97e78
junseongday <junseongday@gmail.com>
2020-05-27 20:24:37 +0900

    nginx setting

--- src/main/java/com/junseong/webservice/web/WebRestController.java
+++ src/main/java/com/junseong/webservice/web/WebRestController.java
@@ -3,13 +3,17 @@ package com.junseong.webservice.web;
 import com.junseong.webservice.dto.posts.PostsSaveRequestDto;
 import com.junseong.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
     private PostsService postsService;
+    private Environment env;
 
     @GetMapping("/hello")
     public String hello() {
@@ -19,5 +23,11 @@ public class WebRestController {
     @PostMapping("/posts")
     public Long savePosts(@RequestBody PostsSaveRequestDto dto) {
         return postsService.save(dto);
+    }
+    @GetMapping("/profile")
+    public String getProfile () {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
--- src/test/java/com/junseong/webservice/web/WebControllerTest.java
+++ src/test/java/com/junseong/webservice/web/WebControllerTest.java
@@ -21,6 +21,14 @@ class WebControllerTest {
 
         //then
         assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
+    }
 
+    @Test
+    public void Profile확인 () {
+        //when
+        String profile = this.restTemplate.getForObject("/profile", String.class);
+
+        //then
+        assertThat(profile).isEqualTo("local");
     }
 }
